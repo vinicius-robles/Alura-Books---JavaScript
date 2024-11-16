fc5ffe1c06971530e317f86b4cea121772aa21fa
vinicius-robles <vinicius.robles@gmail.com>
2024-11-16 16:05:17 -0300

    Adiciona método Reduce para somar o valor total dos livros disponíveis.

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,7 +6,27 @@ function filtrarLivros() {
     const categoria = elementoBtn.value;
     // o valor 'disponivel' é declarado por contexto atravéz da classe 'btnLivrosDisponiveis'
     // porém não não é recomendada a utilização em projetos de escopo grande, de longo prazo ou com muita manutenção.
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
     exibirOsLivrosNaTela(livrosFiltrados);
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
 }
 
